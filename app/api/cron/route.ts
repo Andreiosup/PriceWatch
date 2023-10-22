243c0d7ae47c536d5da8a5e856877b074ae4ec88
Andreiosup <63051479+Andreiosup@users.noreply.github.com>
2023-10-22 15:49:09 +0300

    bug-fix-3

--- app/api/cron/route.ts
+++ app/api/cron/route.ts
@@ -6,6 +6,10 @@ import { getAveragePrice, getEmailNotifType, getHighestPrice, getLowestPrice } f
 import { connect } from "http2"
 import { NextResponse } from "next/server";
 
+export const maxDuration = 300; // This function can run for a maximum of 300 seconds
+export const dynamic = "force-dynamic";
+export const revalidate = 0;
+
 export async function GET () {
     try {
         connectToDB()
